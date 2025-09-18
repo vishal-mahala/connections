b5b63fc03525aa5defaf6b0dfa5ccad2b96120cf
Vishal <vishalmahala6379@gmail.com>
2025-09-18 15:10:36 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Vishal", lastName: "Mahala" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfullyz");
+});
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Vishal", lastName: "Mahala" });
+});
+
 app.get("/hello", (req, res) => {
   res.send("Hello Hello Hello");
 });
