be06ef18396c7cee193d8ae36695dba6ce14fbd5
Vishal <vishalmahala6379@gmail.com>
2025-10-07 22:13:06 +0530

    Adding user updates

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://vishal_mahala24:Vishal%4007@namastenode.mef9z9z.mongodb.net/"
+    "mongodb+srv://vishal_mahala24:Vishal%4007@namastenode.mef9z9z.mongodb.net/connections"
   );
 };
 
